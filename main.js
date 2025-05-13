a6c06b54f075a8cfe3a8028569cf4d38d198dc96
Ongun <info@ongunakay.com>
2025-05-13 17:22:17 +0300

    commit

--- main.js
+++ main.js
@@ -1,13 +1,21 @@
-function getAverage(scores) {
-    let sum = 0;
-
-    for (let i = 0; i < scores.length; i++) {
-        sum += scores[i];
-    }
-
-    let average = sum / scores.length;
-    return average;
+function getGrade(score) {
+  if (score === 100) {
+    return "A++";
+  } else if (score >= 90 && score <= 99) {
+    return "A";
+  } else if (score >= 80 && score <= 89) {
+    return "B";
+  } else if (score >= 70 && score <= 79) {
+    return "C";
+  } else if (score >= 60 && score <= 69) {
+    return "D";
+  } else if (score >= 0 && score <= 59) {
+    return "F";
+  } else {
+    return "Invalid score"; // Optional: to handle unexpected input
+  }
 }
 
-console.log(getAverage([92, 88, 12, 77, 57, 100, 67, 38, 97, 89]));
-console.log(getAverage([45, 87, 98, 100, 86, 94, 67, 88, 94, 95]));
+console.log(getGrade(96)); // A
+console.log(getGrade(82)); // B
+console.log(getGrade(56)); // F
